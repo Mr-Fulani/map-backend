197b6db78d853d77cec980521d209f88c0d1e99d
Anvar <anvar.sharipov.1986@gmail.com>
2026-05-09 15:22:21 +0300

    feat(datasources): зарегистрировать DataSourceConnection в admin

---
+++ apps/datasources/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from unfold.admin import ModelAdmin
+from .models import DataSourceConnection
+
+@admin.register(DataSourceConnection)
+class DataSourceConnectionAdmin(ModelAdmin):
+    list_display = ['name', 'tenant', 'type', 'is_active', 'last_sync_status', 'last_sync_at']
+    list_filter = ['type', 'is_active', 'last_sync_status']
+    search_fields = ['name', 'tenant__name']
+    readonly_fields = ['last_sync_at', 'last_sync_status', 'last_error']
+    exclude = ['credentials']  # Не показываем зашифрованные креды в админке, чтобы не ломался шаблон
