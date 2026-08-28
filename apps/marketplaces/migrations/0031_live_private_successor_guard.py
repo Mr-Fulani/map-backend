47b999b4e764ac28a05061aeaa3b06f165e05dfe
Anvar <anvar.sharipov.1986@gmail.com>
2026-08-28 20:15:17 +0300

    fix(avito): match artifact guard predicate

--- apps/marketplaces/migrations/0031_live_private_successor_guard.py
+++ apps/marketplaces/migrations/0031_live_private_successor_guard.py
@@ -40,13 +40,28 @@ _PRIVATE_OR_LIVE_LEGACY_ENDPOINT = '''OR NOT (
                OR (endpoint_row.storage_mode = 'legacy_bridge'
                    AND endpoint_row.serve_enabled IS TRUE)
            )'''
+_DARK_OR_LEGACY_ARTIFACT_ENDPOINT = '''OR NOT (
+           (endpoint_row.storage_mode = 'private_generation'
+            AND endpoint_row.serve_enabled IS FALSE)
+           OR (endpoint_row.storage_mode = 'legacy_bridge'
+               AND endpoint_row.serve_enabled IS TRUE)
+       )'''
+_PRIVATE_OR_LIVE_LEGACY_ARTIFACT_ENDPOINT = '''OR NOT (
+           endpoint_row.storage_mode = 'private_generation'
+           OR (endpoint_row.storage_mode = 'legacy_bridge'
+               AND endpoint_row.serve_enabled IS TRUE)
+       )'''
 
 if ORIGINAL_UPLOAD_GUARD_SQL.count(_DARK_OR_LEGACY_ENDPOINT) != 2:
     raise RuntimeError(
         '0030 upload guard endpoint predicates changed unexpectedly.',
     )
 
-if ORIGINAL_ARTIFACT_GUARD_SQL.count(_DARK_OR_LEGACY_ENDPOINT) != 1:
+if (
+    ORIGINAL_ARTIFACT_GUARD_SQL.count(
+        _DARK_OR_LEGACY_ARTIFACT_ENDPOINT,
+    ) != 1
+):
     raise RuntimeError(
         '0030 artifact guard endpoint predicate changed unexpectedly.',
     )
@@ -56,8 +71,8 @@ UPDATED_UPLOAD_GUARD_SQL = ORIGINAL_UPLOAD_GUARD_SQL.replace(
     _PRIVATE_OR_LIVE_LEGACY_ENDPOINT,
 )
 UPDATED_ARTIFACT_GUARD_SQL = ORIGINAL_ARTIFACT_GUARD_SQL.replace(
-    _DARK_OR_LEGACY_ENDPOINT,
-    _PRIVATE_OR_LIVE_LEGACY_ENDPOINT,
+    _DARK_OR_LEGACY_ARTIFACT_ENDPOINT,
+    _PRIVATE_OR_LIVE_LEGACY_ARTIFACT_ENDPOINT,
 )
 
 
